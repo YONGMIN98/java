050efb3487dbfe1522cc5d0e3fbd6bdb2b9b6a58
YONGMIN98 <57663455+YONGMIN98@users.noreply.github.com>
2025-04-26 17:19:35 +0900

    Add files via upload

---
+++ for/for1.java
@@ -0,0 +1,9 @@
+package ForWhileSwitch;
+
+public class for1 {
+    public static void main(String[] args){
+        for(int i=1; i<=6;i++){
+            System.out.println("미안해");
+        }
+    }
+}
---
+++ for/for2.java
@@ -0,0 +1,12 @@
+package ForWhileSwitch;
+
+public class for2 {
+    public static void main(String[] args){
+        int sum = 0;
+
+        for(int i=1; i<=5; i++){
+            sum += i;
+            System.out.printf("1부터 %2d 까지의 합: %2d%n",i,sum);
+        }
+        }
+}
